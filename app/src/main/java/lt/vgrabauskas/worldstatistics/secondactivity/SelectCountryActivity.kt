a1afffbe5f9f0252ac82e774842a62c204e2c6a0
vgrabauskas <valdasgrab@gmail.com>
2023-08-02 22:13:57 +0300

    Apply RecyclerView for SelectCountryActivity.kt, adjust UI

--- app/src/main/java/lt/vgrabauskas/worldstatistics/secondactivity/SelectCountryActivity.kt
+++ app/src/main/java/lt/vgrabauskas/worldstatistics/secondactivity/SelectCountryActivity.kt
@@ -3,12 +3,12 @@ package lt.vgrabauskas.worldstatistics.secondactivity
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import lt.vgrabauskas.worldstatistics.CountryAdapter
 import lt.vgrabauskas.worldstatistics.databinding.ActivitySelectCountryBinding
 import lt.vgrabauskas.worldstatistics.repository.Country
 
@@ -17,9 +17,9 @@ class SelectCountryActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySelectCountryBinding
     private lateinit var initialCountry: Country
     private lateinit var selectedCountry: Country
-    private lateinit var countryViewModel: SelectCountryViewModel
-    private lateinit var adapter: ArrayAdapter<String>
-    private lateinit var countriesForComparisonFiltered: List<Country>
+    private lateinit var countryAdapter: CountryAdapter
+    private val countryViewModel: SelectCountryViewModel by viewModels()
+
     private var isCountrySelected = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,12 +29,7 @@ class SelectCountryActivity : AppCompatActivity() {
 
         initialCountry = intent.getParcelableExtra("selectedCountry")!!
 
-        countryViewModel = ViewModelProvider(this).get(SelectCountryViewModel::class.java)
-        countryViewModel.fetchCountries()
-
-        val countriesListView: ListView = binding.countriesListViewComparison
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, mutableListOf<String>())
-        countriesListView.adapter = adapter
+        setupRecyclerView()
 
         val searchView: SearchView = binding.searchView
         searchView.queryHint = "Search countries"
@@ -53,27 +48,27 @@ class SelectCountryActivity : AppCompatActivity() {
             }
         })
 
-        countryViewModel.countryLiveData.removeObservers(this)
-
         val observer = Observer<List<Country>> { countries ->
-            countriesForComparisonFiltered = countries.filter { it != initialCountry }
-            adapter.clear()
-            adapter.addAll(countriesForComparisonFiltered.map { country -> country.commonName })
-            countriesListView.setOnItemClickListener { _, _, position, _ ->
-                val secondSelectedCountry = countriesForComparisonFiltered.getOrNull(position)
-                if (secondSelectedCountry != null) {
-                    selectedCountry =
-                        countries.find { it.commonName == secondSelectedCountry.commonName }
-                            ?: secondSelectedCountry
-                    isCountrySelected = true
-                    startCountryDetailsActivity()
-                }
-            }
+            val countriesForComparisonFiltered = countries.filter { it != initialCountry }
+            countryAdapter.updateData(countriesForComparisonFiltered)
+
             if (!isCountrySelected && ::selectedCountry.isInitialized) {
                 startCountryDetailsActivity()
             }
         }
         countryViewModel.countryLiveData.observe(this, observer)
+        countryViewModel.fetchCountries()
+    }
+
+    private fun setupRecyclerView() {
+        countryAdapter = CountryAdapter { clickedCountry ->
+            selectedCountry = clickedCountry
+            isCountrySelected = true
+            startCountryDetailsActivity()
+        }
+
+        binding.countriesRecyclerViewComparison.adapter = countryAdapter
+        binding.countriesRecyclerViewComparison.layoutManager = LinearLayoutManager(this)
     }
 
     private fun filterCountries(query: String?) {
@@ -81,9 +76,8 @@ class SelectCountryActivity : AppCompatActivity() {
             country.commonName.contains(query ?: "", ignoreCase = true)
         } ?: emptyList()
 
-        countriesForComparisonFiltered = filteredCountries.filter { it != initialCountry }
-        adapter.clear()
-        adapter.addAll(countriesForComparisonFiltered.map { country -> country.commonName })
+        val countriesForComparisonFiltered = filteredCountries.filter { it != initialCountry }
+        countryAdapter.updateData(countriesForComparisonFiltered)
     }
 
     private fun startCountryDetailsActivity() {
